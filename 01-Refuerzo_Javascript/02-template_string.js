eb0c31dbd958acc201e926c79eb69ae3c86e0878
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2023-03-02 20:49:46 -0300

    Template strings

---
+++ 01-Refuerzo_Javascript/02-template_string.js
@@ -0,0 +1,13 @@
+/**
+ * Template string
+ */
+
+const nombre = 'Juan';
+const apellido = 'Perez';
+
+const concatenacion = console.log(`Alumno ${nombre} ${apellido}`);
+
+/**
+ * Con esta esdtructura llamada template string podemos concatenar
+ * varibales con texto dentro de los backtics
+ */

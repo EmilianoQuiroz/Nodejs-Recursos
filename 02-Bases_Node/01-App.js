23ac96fc80b7c6b9834ad24fc842331028e64200
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-03-07 22:53:56 -0300

    Inicializacion de proyecto de consola

---
+++ 02-Bases_Node/01-App.js
@@ -0,0 +1,22 @@
+/**
+ * Tabla del 5 por consola
+ */
+
+/**
+ * Limpiar consola 
+ */
+console.clear();
+
+/**
+ * Mostrar por consola la tabla del 5
+ */
+
+// Base a multiplicar
+let base = 5;
+
+// Ciclo for para generar la tabla que se va a mostrar por pantalla
+for(let i = 1; i <= 10; i++){
+   
+    console.log(`${base} x ${i} = ${base * i}`);
+
+}
